ae1d36097cddddf77609bc88e5ec0e3a2e929308
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-03-13 19:39:03 +0900

    Add files via upload

---
+++ Program4/Class1th (Time & Space complexity)/Class1th (Time & Space complexity).cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 시간 복잡도
+	//	프로그램의 입력값과 연산 수행 시간의 상관관계를 나타내는 척도
+#pragma region 상수 시간 O(1)
+		// 입력 크기와 상관없이 일정한 시간 복잡도를 가지는 시간	
+		// ex) 배열의 요소접근
+
+#pragma endregion
+
+#pragma region 로그 시간 O(log n)
+		//	데이터 크기에 따라 실행 시간이 로그함수 형태로 증가하는 시간 복잡도
+		//	ex) 이진 탐색
+
+#pragma endregion
+
+#pragma region 선형 시간 O(n)
+		//	입력에 따라 시간이 선형적으로 증가되는 시간 복잡도
+		//	ex) 순차 탐색
+
+#pragma endregion
+
+#pragma region 제곱 시간 O(n²)
+		// 입력되는 데이터를 기준으로 n²만큼 시간이	 증가되는 시간 복잡도
+		// ex) 2 for statment
+
+#pragma endregion
+
+
+
+#pragma endregion
+
+#pragma region 공간 복잡도
+	// 프로그램 실행과 완료시 어느정도의 공간이 필요한지 나타내는 척도
+#pragma region 고정 공간
+		//	코드가 저장되는 공간, 알고리즘 실행을 위해 시스템이 필요하는 공간
+		// ex) 배열
+#pragma endregion
+
+#pragma region 가변 공간
+		// 문제 해결을 위해 알고리즘이 필요하는 공간
+		// ex) 재귀함수
+#pragma endregion
+
+
+
+#pragma endregion
+
+#pragma region 점근적 표기법
+	// 상수 계수와 중요하지 않은 항목을 제거한 표기법
+	// ex) f(n) = n²+n+100 일 때 최고차항 만 표시 f(n) = n²
+#pragma endregion
+
+
+	return 0;
+}
---
+++ Program4/Program4/Program4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 완전 탐색 (브루트 포스)
+	// 가능한 모든 경우의 수를 탐색하여 결과를 찾는 알고리즘
+
+	int pw[] = {7, 1, 2};
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				if (pw[0] == i && pw[1] == j && pw[2] == k)
+				{
+					cout << "Password : " << i << " " << j << " " << k << endl;
+					break;
+				}
+			}
+		}
+	}
+#pragma endregion
+
+
+	return 0;
+}
