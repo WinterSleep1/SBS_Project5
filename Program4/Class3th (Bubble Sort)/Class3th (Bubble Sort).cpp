bb5eff0e847901498c3055b5b6b23e4488f4c45a
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-03-14 19:42:16 +0900

    Add files via upload

---
+++ Program4/Class3th (Bubble Sort)/Class3th (Bubble Sort).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+#pragma region 거품정렬
+	// 인접한 두 원소를 비교하여 정렬하는 알고리즘
+	int list[SIZE] = { 13, 8, 97, 1, 36 };
+	int a;
+
+	for (int i = SIZE; i > 0; i--)
+	{
+		for (int j = 0; j < i - 1; j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				a = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = a;
+			}
+		}
+	}
+
+	for (int i = 0; i < SIZE; i++) { cout << list[i] << " "; }
+
+#pragma endregion
+
+
+	return 0;
+}
---
+++ Program4/Class4th (Select Sort)/Class4th (Select Sort).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+	// 리스트 중에서 최소값을 찾아 특정한 위치에서 결과를 교체하는 알고리즘
+	int list[SIZE] = { 6, 2, 8, 1, 0 };
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		int min = list[i];
+		int index = i;
+
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (min > list[j])
+			{
+				min = list[j];
+				index = j;
+			}
+		}
+
+		swap(list[i], list[index]);
+	}
+
+	for (int i = 0; i < SIZE; i++) { cout << list[i] << " "; }
+#pragma endregion
+
+
+	return 0;
+}
